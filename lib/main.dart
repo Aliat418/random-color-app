8c95fe5f202164f99cdf9081c0cb26f2986d6327
Aliat418 <aliat418@icloud.com>
2024-11-28 12:07:14 +0200

    Refactored imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'core/infrastructure/di.dart';
 import 'presentation/app/random_color_app.dart';
+import 'core/infrastructure/di.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
